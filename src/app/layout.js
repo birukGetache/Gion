cd46d4761207530ff1009dc239a83e8bee0f9787
Biruk Getachew <156351009+birukGetache@users.noreply.github.com>
2025-01-19 22:40:35 -0800

    Update layout.js chat bot with out useeffect

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
-'use client'
-import { useEffect } from "react";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,30 +18,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    // Dynamically inject Tawk.to script
-    const Tawk_API = Tawk_API || {};
-    const Tawk_LoadStart = new Date();
-
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://embed.tawk.to/678de9003a842732607179b2/1ii15cblb";
-    script.charset = "UTF-8";
-    script.setAttribute("crossorigin", "*");
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup script if needed
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark-theme`}
       >
         {children}
+        <Script
+          src="https://embed.tawk.to/678de9003a842732607179b2/1ii15cblb"
+          strategy="afterInteractive" // Load script after page is interactive
+        />
       </body>
     </html>
   );
