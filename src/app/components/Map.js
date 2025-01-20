a30034eaf2bdf178f20ec510bd36ee7694c09af7
Biruk Getachew <156351009+birukGetache@users.noreply.github.com>
2025-01-20 02:15:07 -0800

    Update Map.js

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix default marker icon issue in Leaflet
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet styles
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const MapComponent = () => {
-  // Coordinates for Bahir Dar, Ethiopia
-     center={[11.3620, 37.2433]}
-  const zoom = 13; // Zoom level
-
   return (
-    <MapContainer center={position} zoom={zoom} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          A simple marker at coordinates {position[0]}, {position[1]} (Bahir Dar).
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="relative" style={{ height: '100vh', width: '100%' }}>
+      <MapContainer
+        center={[11.3620, 37.2433]} // Correct coordinates for Bahir Dar, Ethiopia
+        zoom={13} // Zoom level
+        style={{ height: '100%', width: '100%' }}
+      >
+        {/* Add a dark-themed tile layer */}
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/attributions">CartoDB</a> contributors'
+        />
+
+        {/* Add a marker */}
+        <Marker position={[11.3620, 37.2433]}>
+          <Popup>
+            Welcome to Bahir Dar! <br /> A beautiful city in Ethiopia.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      
+      {/* Slate Gray Overlay */}
+      <div className="absolute top-0 left-0 w-full h-full bg-slate-700 opacity-50"></div>
+    </div>
   );
 };
 
